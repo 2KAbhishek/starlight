91a1298e6f3116714c8e819954cc4d71d8362160
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-07-31 23:14:50 +0530

    Add dummy main

---
+++ src/main/java/starlight/App.java
@@ -0,0 +1,13 @@
+package starlight;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
