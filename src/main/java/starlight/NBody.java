037b1b5445fa2ebe3fcdb2346e55a451df2ebc34
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-07-31 23:17:13 +0530

    Add NBody

---
+++ src/main/java/starlight/NBody.java
@@ -0,0 +1,57 @@
+package starlight;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class NBody extends Canvas implements ActionListener {
+    public int n, x, y, size;
+    public double dt, maxVel, maxMass;
+
+    public void init(int n) {
+        this.n = n;
+        x = 0;
+        y = 0;
+    }
+
+    // Draw a circle of radius r at (x,y)
+    public void drawCircle(Graphics g, int x, int y, int r) {
+        int d = 2 * r;
+        g.fillOval(x - r, y - r, d, d);
+    }
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.setColor(Color.WHITE);
+        drawCircle(g, x, y, 10);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        x++;
+        y++;
+        repaint();
+        Toolkit.getDefaultToolkit().sync();
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("NBody");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        int n = Integer.parseInt(args[0]);
+
+        NBody nbody = new NBody();
+        nbody.setBackground(Color.BLACK);
+        nbody.size = 800;
+        nbody.maxVel = 10;
+        nbody.maxMass = 10;
+        nbody.dt = 0.1;
+        nbody.setPreferredSize(new Dimension(nbody.size, nbody.size));
+        nbody.init(n);
+        frame.add(nbody);
+        frame.pack();
+
+        Timer timer = new Timer(10, nbody);
+        timer.start();
+
+        frame.setVisible(true);
+    }
+}
